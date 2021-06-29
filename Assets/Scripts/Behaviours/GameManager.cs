796a7d27e968e98f7895782aaaa618f3c8b66872
Michal Vyšinský <michal@vysinsky.cz>
2021-06-29 17:45:07 +0200

    Player movement

---
+++ Assets/Scripts/Behaviours/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace games.almost_purrfect.fastcube.behaviours
+{
+    public class GameManager : MonoBehaviour
+    {
+
+        [SerializeField]
+        private GameObject player;
+
+        private void Update()
+        {
+            if (player != null && player.transform.position.y < -10f)
+            {
+                StartCoroutine(GameOver());
+            }
+        }
+
+        private IEnumerator GameOver()
+        {
+            Destroy(player);
+            yield return new WaitForSeconds(1f);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Behaviours/Player.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.PlayerLoop;
+
+namespace games.almost_purrfect.fastcube.behaviours
+{
+    public class Player : MonoBehaviour
+    {
+
+        public List<Vector2> _inputBuffer;
+
+        [SerializeField]
+        private float moveDistance;
+
+        [SerializeField]
+        private float moveDuration;
+
+        [SerializeField]
+        private LayerMask groundCheckLayerMask;
+
+        private Vector3 _originalPosition;
+
+        private Vector3 _targetPosition;
+
+        private bool _isMoving;
+
+        private bool _isGrounded;
+
+        private float _currentMoveTime;
+
+        private void Start()
+        {
+            _inputBuffer = new List<Vector2>(5);
+            _originalPosition = _targetPosition = transform.position;
+        }
+
+        private void FixedUpdate()
+        {
+            var t = transform;
+            _isGrounded = Physics.Raycast(t.position, Vector3.down, 1f, groundCheckLayerMask);
+        }
+
+        private void Update()
+        {
+            var dt = Time.deltaTime;
+            var t = transform;
+
+            if (_inputBuffer.Count > 0)
+            {
+                if (!_isMoving)
+                {
+                    var currentInput = _inputBuffer[0];
+
+                    if (math.abs(currentInput.x) > .1f)
+                    {
+                        _currentMoveTime = 0f;
+                        _originalPosition = t.position;
+                        _targetPosition = _originalPosition +
+                                          new Vector3(
+                                              currentInput.x > 0f ? moveDistance : -moveDistance, 0f, 0f);
+                    }
+
+                    if (math.abs(currentInput.y) > .1f)
+                    {
+                        _currentMoveTime = 0f;
+                        _originalPosition = t.position;
+                        _targetPosition = _originalPosition +
+                                          new Vector3(0f, 0f,
+                                              currentInput.y > 0f ? moveDistance : -moveDistance);
+                    }
+
+                    _inputBuffer.RemoveAt(0);
+                }
+            }
+
+            if (_isGrounded)
+            {
+                if (_currentMoveTime <= moveDuration)
+                {
+                    _currentMoveTime += dt;
+                    t.position = math.lerp(_originalPosition, _targetPosition,
+                        _currentMoveTime / moveDuration);
+                    _isMoving = true;
+                }
+                else
+                {
+                    t.position = _targetPosition;
+                    _isMoving = false;
+                }
+            }
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            if (!context.performed) return;
+
+            var input = context.ReadValue<Vector2>();
+            _inputBuffer.Add(input);
+        }
+    }
+}
