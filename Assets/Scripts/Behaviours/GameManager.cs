5f822fe570ffaf6d5a157a8d247aca3f14cb6d46
Michal Vyšinský <michal@vysinsky.cz>
2021-07-11 00:30:11 +0200

    feat: add Google Play Games services

--- Assets/Scripts/Behaviours/GameManager.cs
+++ Assets/Scripts/Behaviours/GameManager.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,11 +9,41 @@ namespace games.almost_purrfect.fastcube.behaviours
 {
     public class GameManager : MonoBehaviour
     {
-
         public static bool LevelReady;
 
-        [SerializeField]
-        private GameObject player;
+        [SerializeField] private GameObject player;
+
+        private void InitializeGooglePlayServices()
+        {
+            var config = new PlayGamesClientConfiguration.Builder()
+                .RequestServerAuthCode(false)
+                .Build();
+
+            PlayGamesPlatform.InitializeInstance(config);
+            PlayGamesPlatform.DebugLogEnabled = true;
+            PlayGamesPlatform.Activate();
+            Debug.Log("Google Play Games initialized");
+        }
+
+        private void Start()
+        {
+            InitializeGooglePlayServices();
+
+            PlayGamesPlatform.Instance.Authenticate(SignInInteractivity.CanPromptOnce, (result) =>
+            {
+                Debug.Log("Google Play sign in result");
+                Debug.Log(result);
+
+                if (result == SignInStatus.Success)
+                {
+                    Social.ReportProgress("CgkI5_2m85oQEAIQAg", 100.0, (r) =>
+                    {
+                        Debug.Log("Achievement reported");
+                        Debug.Log(r);
+                    });
+                }
+            });
+        }
 
         private void Update()
         {
