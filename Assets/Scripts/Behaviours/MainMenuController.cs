9c987619d71308ee29fdf0286b9863cb7849adf7
Michal Vyšinský <michal@vysinsky.cz>
2021-09-07 00:00:21 +0200

    feat: improved logo, version label, improved UI placement

--- Assets/Scripts/Behaviours/MainMenuController.cs
+++ Assets/Scripts/Behaviours/MainMenuController.cs
@@ -1,9 +1,16 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 namespace games.almost_purrfect.fastcube.behaviours
 {
     public class MainMenuController : MonoBehaviour
     {
-        
+
+        [SerializeField] private TextMeshProUGUI versionLabel;
+
+        private void Start()
+        {
+            versionLabel.text = $"v{Application.version}";
+        }
     }
 }
